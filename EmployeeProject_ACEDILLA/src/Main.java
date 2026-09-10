e409131db9878a79c5e85e31e0b2b300e9eff9da
d4nn1yammy <acedilladanniah@gmail.com>
2026-09-10 10:37:34 +0800

    Add files via upload
    added project files

---
+++ EmployeeProject_ACEDILLA/src/Main.java
@@ -0,0 +1,12 @@
+import version1.HourlyEmployee;
+public class Main{
+
+public static void main(String[] args) {
+    HourlyEmployee h1 = new HourlyEmployee();
+    HourlyEmployee h2 = new HourlyEmployee(12, "Maria");
+    HourlyEmployee h3 = new HourlyEmployee(5, "Maria", 8, 12 );
+
+
+    System.out.println(h3);
+        }
+    }
---
+++ EmployeeProject_ACEDILLA/src/version1/PieceWorkerEmployee.java
@@ -0,0 +1,61 @@
+package version1;
+
+public class PieceWorkerEmployee {
+    private int empID;
+    private String empName;
+    private int totalPiecesFinished;
+    private double ratePerPiece;
+
+    public PieceWorkerEmployee() {
+        this(0,"N/A");
+    }
+
+    public PieceWorkerEmployee(int empID, String empName) {
+        this(empID, empName, 0, 0);
+    }
+
+    public PieceWorkerEmployee(int empID, String empName, int totalPiecesFinished, double ratePerPiece){
+        this.empID = empID;
+        this.empName = empName;
+        this.totalPiecesFinished = totalPiecesFinished;
+        this.ratePerPiece = ratePerPiece;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public int getTotalPiecesFinished() {
+        return totalPiecesFinished;
+    }
+
+    public void setTotalPiecesFinished(int totalPiecesFinished) {
+        this.totalPiecesFinished = totalPiecesFinished;
+    }
+
+    public double getRatePerPiece() {
+        return ratePerPiece;
+    }
+
+    public void setRatePerPiece(double ratePerPiece) {
+        this.ratePerPiece = ratePerPiece;
+    }
+
+    public double computeSalary(){
+        double BasePay = totalPiecesFinished * ratePerPiece;
+
+
+    }
+}
